50862e5641f8e38f1985670a957c1fd22a5bd4f3
jin <jintyjin15@gmail.com>
2023-12-07 16:32:26 +0900

    init

---
+++ src/main/java/hells/care/HellsCareApplication.java
@@ -0,0 +1,13 @@
+package hells.care;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HellsCareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HellsCareApplication.class, args);
+	}
+
+}
