30a9418c0595ee9d0fb1e6f43c2f65b4fcbf2020
jin <jintyjin15@gmail.com>
2023-12-14 17:51:06 +0900

    feat: UserRepository  - add two find method(loginId, nickName)

---
+++ src/main/java/hells/care/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package hells.care.repository;
+
+import hells.care.entity.user.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByLoginId(String loginId);
+
+    Optional<User> findByNickName(String nickName);
+}
