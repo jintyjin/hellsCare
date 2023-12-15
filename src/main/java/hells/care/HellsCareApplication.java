efae17d86fde56d40f60574c14762f6bf7e8304f
jin <jintyjin15@gmail.com>
2023-12-15 15:51:41 +0900

    chore: add Jpa Auditing annotation

--- src/main/java/hells/care/HellsCareApplication.java
+++ src/main/java/hells/care/HellsCareApplication.java
@@ -2,8 +2,10 @@ package hells.care;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class HellsCareApplication {
 
 	public static void main(String[] args) {
